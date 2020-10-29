3373cac78074f6da4530dcb46a3eca5bf86b02a8
Ivan Burkov <pobednik@bk.ru>
2020-10-29 19:23:05 +0300

    Done

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,31 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	// реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physic = p;
+	let average = (algebra + geography + physic) / 3;
     
-	return // ваш ответ;
+	return average;// ваш ответ;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	// реализуйте здесь задание №2
+	let myName = userName;
+	let message = (`Привет, мир! Меня зовут ${myName}`);
+
     
-	return // ваш ответ;
+	return message;// ваш ответ;
 }
 
 function calculateFormula() {
     
 	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let equation = x * y + 5 * z + x - 1;
+    return equation;// ваш ответ;
 }
