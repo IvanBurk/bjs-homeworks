86aec35aec808b8cdd24b607ed70200281af24a5
Ivan Burkov <pobednik@bk.ru>
2020-11-03 20:42:52 +0300

    Done2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,13 @@
+"use strict";
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    "use strict";
+    
     let d = b**2 - 4*a*c;
     let x = [];
-    if (d < 0) {
-      x;
-    } else if (d === 0) {
+     if (d === 0) {
         x[0] = (-b)/2*a;
     }
- else {
+ else if (d > 0){
     x[0] = ((-b) + Math.sqrt(b*b - 4*a*c))/2*a;
     x[1] = ((-b) - Math.sqrt(b*b - 4*a*c))/2*a;
 }
@@ -17,35 +16,25 @@ return x;
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
-   let quantity = marks.length;
-    if (quantity === 0) {
-        return quantity;
-    } else if (quantity > 5) {
-        
+    if (marks.length === 0) {
+        return marks.length;
+    } else if ((marks.length > 5) || (marks.length <= 5)) {
         let sum = 0;
-        for (let i = 0; i < 5; i++)
+        for (let i = 0; i < marks.slice(0,5).length; i++) 
         sum += marks[i];
-        let middle = sum/5;
-        return middle;
-    } else {
-        let sum = 0;
-        for (let i = 0; i < marks.length; i++)
-        sum += marks[i];
-        let middle = sum/marks.length;
-        return middle;
-    }
+        return sum/marks.slice(0,5).length;
+    } 
     
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
     let result;
-    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
-    if (age > 18) {
-        result = `Не желаете ли олд-фэшн, ${name}?`;            
+    if ((new Date().getFullYear() - dateOfBirthday.getFullYear()) > 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`;            
     } else {
-        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     }
-    return result;
+
     // return result;
 }
