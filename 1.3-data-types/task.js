3999cf606b867e3ca3d10941e9484fdd95084a73
Ivan Burkov <pobednik@bk.ru>
2020-11-08 00:03:44 +0300

    Test2

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,21 +1,36 @@
 "use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    let boDy = amount - contribution;
+    if (percent === Number(percent)) {
+          percent === new Number(percent);
+    } else {
+        return `Параметр Процентная ставка содержит неправильное значение ${percent}`; 
+    };
+
+    if (contribution === Number(contribution)) {
+        contribution === new Number(contribution);
+  } else {
+      return `Параметр Начальный взнос содержит неправильное значение ${contribution}`; 
+  };
+  if (amount === Number(amount)) {
+    amount === new Number(amount);
+} else {
+  return `Параметр Общая стоимость содержит неправильное значение ${amount}`; 
+};
+    let boDy = new Number(amount - contribution);
     let month = date.getMonth() - new Date().getMonth() + (12 * (date.getFullYear() - new Date().getFullYear()));
-    let p = percent/100;
-    let payment = new Number(boDy*(p + p/(((1 + p)**month) - 1)));
-    return payment.toFixed(2);
+    let p = new Number(percent/100/12);
+    let payment = new Number((boDy*(p + p/(((1 + p)**month) - 1)))*month);
+    return Number(payment.toFixed(2));
     // return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    let nameP = new String(name);
-    if (nameP === "Аноним") {
-        return `Привет, мир! Меня зовут Аноним`;
+    if ((name === "Аноним") || (name === "") || (name === null) || (name === undefined) || (name === " ")) {
+        return `Привет мир, меня зовут Аноним`;
     } else {
-        return `Привет, мир! Меня зовут ${nameP}`;
-    }
+        return `Привет мир, меня зовут ${name}`;
+    };
     // return greeting;
 }
