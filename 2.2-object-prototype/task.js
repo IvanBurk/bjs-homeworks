488726da586e6210e16fa76c3120bebb6570482f
Ivan Burkov <ivannburk@gmail.com>
2020-11-22 21:20:19 +0300

    Third

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,19 +1,32 @@
 //String.prototype.isPalindrome - для задачи №1
-String.prototype.isPalindrome = function isPalindrome(String) {
-  this.String = String
-}; 
+String.prototype.isPalindrome = function isPalindrome() {
+  const str = this;
+  str = str.toLowerCase();
+  let len = str.length;
+   let  rev = str.split('').reverse().join('');
+    if (rev === str) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+      // ‘еле-еле’.isPalindrome();
+
+
+
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     let sum = 0;
     for (let i = 0; i < marks[i]; i++)
-    sum += marks[i];
-    let average = sum/marks.length;
-    let averageMark = Math.round(average);
-    if (marks.length === 0) {
-      return 0;
-       } else {
-    return averageMark;
+      sum += marks[i];
+      let average = sum/marks.length;
+      let averageMark = Math.round(average);
+        if (marks.length === 0) {
+              return 0;
+            } else {
+              return averageMark;
        }
     // return averageMark
 }
